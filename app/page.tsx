f3faeee3bc4664c4eb2dfa34d50fefa1e251d74c
lwev <106478077+lwev@users.noreply.github.com>
2026-04-29 18:11:54 +0200

    ,update3“

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,6 @@ export default function LandingPage() {
       <FeaturesSection />
       <AppScreenshotsSection />
       <StoresSection />
-      <ProspekteSection />
       <CategoriesSection />
       <HowItWorksSection />
       <FAQSection />
